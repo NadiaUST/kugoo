c9929c7a07bc5f1badcbf7abd03d3dd11b1031b0
NadiaUST <ustinova7n@gmail.com>
2025-10-20 12:32:13 -0500

    create: added handler.php

--- js/main.js
+++ js/main.js
@@ -19,3 +19,121 @@ document.addEventListener("keyup", (event) => {
     modal.classList.toggle("is-open");
   }
 });
+
+// валидация для форм
+const forms = document.querySelectorAll("form"); // собираем формы
+forms.forEach((form) => {
+  const validation = new JustValidate(form, {
+    errorFieldCssClass: ".is-invalid",
+  });
+  validation
+    .addField("[name=phone]", [
+      {
+        rule: "required",
+        errorMessage: "Укажите телефон",
+      },
+    ])
+    // отправка данных на сервер
+    .onSuccess((event) => {
+      const thisForm = event.target; // наша форма
+      const formData = new FormData(thisForm); // данные из нашей формы
+      const ajaxSend = (formData) => {
+        fetch(thisForm.getAttribute("action"), {
+          method: thisForm.getAttribute("method"),
+          body: formData,
+        }).then((response) => {
+          if (response.ok) {
+            thisForm.reset();
+            currentModal.classList.remove("is-open");
+            alertModal.classList.add("is-open");
+            currentModal = alertModal;
+            modalDialog = currentModal.querySelector(".modal-dialog");
+            // отслеживаем клик по окну и пустым областям
+            currentModal.addEventListener("click", (event) => {
+              // если клик в пустую область (не диалог)
+              if (!event.composedPath().includes(modalDialog)) {
+                // то закрываем окно
+                currentModal.classList.remove("is-open");
+              }
+            });
+          } else {
+            // если не прошло, то выходит форма ошибки
+            alert("Ошибка. Текс ошибки: ".response.statusText);
+          }
+        });
+      };
+      ajaxSend(formData);
+    });
+});
+
+// Маска ввода номера телефона
+
+/* Создаем префикс +7, даже если вводят 8 или 9 */
+const prefixNumber = (str) => {
+  /* если вводят семерку, добавляем ей скобку */
+  if (str === "7") {
+    return "7 (";
+  }
+  /* если вводят восьмерку, ставим вместо нее +7 ( */
+  if (str === "8") {
+    return "+7 (";
+  }
+  /* если пишут девятку, заменяем на +7 (9  */
+  if (str === "9") {
+    return "7 (9";
+  }
+  /* в других случаях просто 7 (  */
+  return "7 (";
+}; /* профикс в любом раскладе будет +7 () */
+
+/* Ловим события ввода в любом поле */
+document.addEventListener("input", (e) => {
+  /* Проверяем, что это поле имеет класс phone-mask */
+  if (e.target.classList.contains("phone-mask")) {
+    /* поле с телефоном помещаем в переменную input */
+    const input = e.target;
+    /* вставляем плюс в начале номера */
+    const value = input.value.replace(/\D+/g, "");
+    /* длинна номера 11 символов */
+    const numberLength = 11;
+
+    /* Создаем переменную, куда будем записывать номер */
+    let result;
+    /* Если пользователь ввел 8... */
+    if (input.value.includes("+8") || input.value[0] === "8") {
+      /* Стираем восьмерку */
+      result = "";
+    } else {
+      /* Оставляем плюсик в поле */
+      result = "+";
+    }
+
+    /* Запускаем цикл, где переберем каждую цифру от 0 до 11 */
+    for (let i = 0; i < value.length && i < numberLength; i++) {
+      switch (i) {
+        case 0:
+          /* в самом начале ставим префикс +7 ( */
+          result += prefixNumber(value[i]);
+          continue;
+        case 4:
+          /* добавляем после "+7 (" круглую скобку ")" */
+          result += ") ";
+          break;
+        case 7:
+          /* дефис после 7 символа */
+          result += "-";
+          break;
+        case 9:
+          /* еще дефис  */
+          result += "-";
+          break;
+        default:
+          break;
+      }
+      /* на каждом шаге цикла добавляем новую цифру к номеру */
+      result += value[i];
+    }
+    /* итог: номер в формате +7 (999) 123-45-67 */
+    input.value = result;
+  }
+});
